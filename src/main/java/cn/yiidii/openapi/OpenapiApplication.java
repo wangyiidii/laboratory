b3eac20e663548335ffd3d574a436f179279c7fe
wangyidi0731 <wangyiidii@163.com>
2021-08-18 21:50:06 +0800

    组件扫描

--- src/main/java/cn/yiidii/openapi/OpenapiApplication.java
+++ src/main/java/cn/yiidii/openapi/OpenapiApplication.java
@@ -3,6 +3,7 @@ package cn.yiidii.openapi;
 import cn.yiidii.pigeon.common.knife4j.annotation.EnablePigeonKnife4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Openapi启动类
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2021/6/1 9:44:38
  */
 @SpringBootApplication
+@ComponentScan(basePackages = "cn.yiidii")
 @EnablePigeonKnife4j
 public class OpenapiApplication {
 
